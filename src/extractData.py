99657a270281c588c7045960e2f136a755df5397
Panagiotis Sklidas <panagiotis.skl@gmail.com>
2023-06-11 21:16:02 +0300

    Update extractData terminal app
    Added -f and --filepath

--- src/extractData.py
+++ src/extractData.py
@@ -64,7 +64,7 @@ def writedatacsv(data):
 
 def main():
     parser = argparse.ArgumentParser(description='Convert KML data to CSV.')
-    parser.add_argument('filepath', type=str, help='Path to the KML file')
+    parser.add_argument('-f', '--filepath', type=str, help='Path to the KML file')
 
     args = parser.parse_args()
 
